c563cd0ff899ccf0cc538a5050b7a1eabfacdf21
cwreed <connor.william.reed@gmail.com>
2022-01-06 13:48:55 -0500

    cleaning up, keeping only Admin 2 analyses, adding FDR

--- Analysis/Monthly_Subnational/Granger-Causality.R
+++ Analysis/Monthly_Subnational/Granger-Causality.R
@@ -19,23 +19,10 @@ panel_data <- as_tibble(data) %>%
   rename(total_flood_dur_deseasoned_diff = total_flood_dur_deseasoned_ret) %>%
   pdata.frame(index = c('FIDcalc', 'time_index'))
   
-panel_data <- as_tibble(data) %>%
-  select(-contains(c("geom", "flood_1", "flood_29", "flood_15"))) %>%
-  # filter(Name != 'Mauritania') %>%
-  mutate(place = as.factor(paste(Name, ADMIN1, sep = "_")),
-  		 place_index = as.numeric(place)) %>%
-  group_by(place_index) %>% 
-  arrange(place_index, datetime_start) %>%
-  mutate(time_index = as.numeric(as.factor(datetime_start)),
-  		 logIPC = log(mean_ipc)) %>%
-  rename(total_flood_dur_deseasoned_diff = total_flood_dur_deseasoned_ret) %>%
-  pdata.frame(index = c('place_index', 'time_index'))
-
 ##########################################  
 # Testing for cross-sectional dependence #
 ##########################################
 pcdtest(granger_data$mean_ipc, test = 'cd', na.action = na.omit)
-
 
 ##########################################
 # Testing for panel unit roots ###########
@@ -55,7 +42,6 @@ cips_results_ipc_diff <- vector(mode = "list", length = length(test_data))
 cips_results_ipc_deseasoned <- vector(mode = "list", length = length(test_data))
 cips_results_ipc_deseasoned_diff <- vector(mode = "list", length = length(test_data))
 
-# ADMIN 3
 for (i in 1:length(test_data)) {
 	test_data_selected <- test_data[[i]] %>%
 							filter(FIDcalc != 1062) # Only 7 seasons of data
@@ -98,71 +84,21 @@ for (i in 1:length(test_data)) {
 ### None of the flood transformations are stationary across the board, but the first-differenced and deseasoned
 ### first-differences are the best.
 
-# ADMIN 1
-test_data <- panel_data %>% 
-	group_by(place_index) %>%
-	nest() %>%
-	split(rep(1:ceiling(273/150), each = 150)[1:273]) %>%
-	lapply(as_tibble) %>%
-	lapply(unnest, cols = c(data)) %>%
-	lapply(function(df) {pdata.frame(df, index = c("place_index", "time_index"))})
-
-for (i in 1:length(test_data)) {
-	test_data_selected <- test_data[[i]]
-	
-	cips_results_ipc[[i]] <- cipstest(panel_data$mean_ipc, lags = 4, na.rm = T)
-	cips_results_logipc[[i]] <- cipstest(panel_data$logIPC, lags = 4, na.rm = T)
-	cips_results_ipc_diff[[i]] <- cipstest(panel_data$mean_ipc_diff, lags = 4, na.rm = T)
-	cips_results_ipc_deseasoned[[i]] <- cipstest(panel_data$mean_ipc_deseasoned, lags = 4, na.rm = T)
-	cips_results_ipc_deseasoned_diff[[i]] <- cipstest(panel_data$mean_ipc_deseasoned_diff, lags = 4, na.rm = T)
-}
-
-test_data <- panel_data %>%
-	group_by(FIDcalc) %>%
-	filter((sum(no_flood_events != 0, na.rm = T) >= 4) & (sum(mean_ipc_diff != 0, na.rm = T) >= 5)) %>%
-	pdata.frame(index = c('FIDcalc', 'time_index'))
-
-cipstest(granger_data$total_flood_dur_deseasoned_diff, lags = 4, na.rm = T)
-
-### All IPC variables are stationary according to CIPS tests
-
-cips_results_floods <- vector(mode = "list", length = length(test_data))
-cips_results_floods_diff <- vector(mode = "list", length = length(test_data))
-cips_results_floods_deseasoned <- vector(mode = "list", length = length(test_data))
-cips_results_floods_deseasoned_diff <- vector(mode = "list", length = length(test_data))
-
-for (i in 1:length(test_data)) {
-	test_data_selected <- test_data[[i]]
-	
-	cips_results_floods[[i]] <- cipstest(test_data_selected$total_flood_area_std, lags = 4, na.rm = T)
-	cips_results_floods_diff[[i]] <- cipstest(test_data_selected$total_flood_area_diff, lags = 4, na.rm = T)
-	cips_results_floods_deseasoned[[i]] <- cipstest(test_data_selected$total_flood_area_deseasoned, lags = 4, na.rm = T)
-	cips_results_floods_deseasoned_diff[[i]] <- cipstest(test_data_selected$total_flood_area_deseasoned_diff, lags = 4, na.rm =T)
-}
-
 
 #####################
 # Granger causality #
 #####################
-
-# ADMIN 2
 granger_data <- panel_data %>%
 	group_by(FIDcalc) %>%
 	filter((sum(no_flood_events != 0, na.rm = T) >= 4) & (sum(mean_ipc_diff != 0, na.rm = T) >= 5)) %>%
 	pdata.frame(index = c('FIDcalc', 'time_index'))
-	
-# ADMIN 1
-granger_data <- panel_data %>%
-	group_by(place_index) %>%
-	filter((sum(no_flood_events != 0, na.rm = T) >= 3) & (sum(mean_ipc_diff != 0, na.rm = T) >= 2)) %>%
-	pdata.frame(index = c('place_index', 'time_index'))
 
 	
-granger_res_nfloods <- pgrangertest(mean_ipc_deseasoned_diff ~ no_floods_deseasoned_diff, data = granger_data, order = 4L)
+granger_res_nfloods <- pgrangertest(mean_ipc_diff ~ no_floods_diff, data = granger_data, order = 4L)
 
-granger_res_flood_dur <- pgrangertest(mean_ipc_deseasoned_diff ~ total_flood_dur_deseasoned_diff, data = granger_data, order = 4L)
+granger_res_flood_dur <- pgrangertest(mean_ipc_diff ~ total_flood_dur_diff, data = granger_data, order = 4L)
 	
-granger_res_flood_area <- pgrangertest(mean_ipc_deseasoned_diff ~ total_flood_area_deseasoned_diff, data = granger_data, order = 4L)
+granger_res_flood_area <- pgrangertest(mean_ipc_diff ~ total_flood_area_diff, data = granger_data, order = 4L)
 
 # All null hypotheses are very confidently rejected, suggesting floods, as measured in various ways, do Granger cause changes in average IPC for a panel, at least heterogeneously
 
@@ -170,9 +106,22 @@ print(granger_res_nfloods)
 print(granger_res_flood_dur)
 print(granger_res_flood_area)
 
+# Determine FDR p-value for significance
+granger_all_pvalues <- rbindlist(list(granger_res_nfloods$indgranger,
+                                       granger_res_flood_dur$indgranger,
+                                       granger_res_flood_area$indgranger))
+
+granger_all_pvalues <- granger_all_pvalues[order(`p-value`),]
+alpha_fdr = 0.10
+granger_all_pvalues <- granger_all_pvalues %>%
+  mutate(p_index = row_number(),
+         bool_fdr = (`p-value` <= (p_index / dim(granger_all_pvalues)[1]) * alpha_fdr))
+
+p_fdr <- max(granger_all_pvalues[granger_all_pvalues$bool_fdr == TRUE, `p-value`])
+
 # Export Granger results for mapping
 
-# ADMIN 3
+# ADMIN 2
 
 granger_pvalues_nfloods <- granger_res_nfloods$indgranger %>%
 					mutate(FIDcalc = as.numeric(as.character(FIDcalc))) %>%
@@ -196,56 +145,17 @@ granger_pvalues_geom <- data %>%
 						left_join(granger_pvalues_flood_dur, by = 'FIDcalc') %>%
 						left_join(granger_pvalues_flood_area, by = 'FIDcalc') %>%
 						filter(time_index == 16) %>%
-						mutate(sig_nfloods = case_when(`p-value_nfloods` < 0.05 ~ 'Yes',
-												`p-value_nfloods` >= 0.05 ~ 'No',
+						mutate(sig_nfloods = case_when(`p-value_nfloods` < p_fdr ~ 'Yes',
+												`p-value_nfloods` >= p_fdr ~ 'No',
 												is.na(`p-value_nfloods`) ~ 'Not enough information'),
-								sig_flood_dur = case_when(`p-value_flood_dur` < 0.05 ~ 'Yes',
-												`p-value_flood_dur` >= 0.05 ~ 'No',
+								sig_flood_dur = case_when(`p-value_flood_dur` < p_fdr ~ 'Yes',
+												`p-value_flood_dur` >= p_fdr ~ 'No',
 												is.na(`p-value_flood_dur`) ~ 'Not enough information'),
-								sig_flood_area = case_when(`p-value_flood_area` < 0.05 ~ 'Yes',
-												`p-value_flood_area` >= 0.05 ~ 'No',
+								sig_flood_area = case_when(`p-value_flood_area` < p_fdr ~ 'Yes',
+												`p-value_flood_area` >= p_fdr ~ 'No',
 												is.na(`p-value_flood_area`) ~ 'Not enough information'),) %>%
 						select(c(Name, ADMIN1, ADMIN2, FIDcalc, `p-value_nfloods`, `p-value_flood_dur`,
 								`p-value_flood_area`, sig_nfloods, sig_flood_dur, sig_flood_area, geom))
 								
-granger_outfile <- "../../Data/granger_results_test_adm3_geom.gpkg"
-st_write(granger_pvalues_geom, granger_outfile)
-
-# ADMIN 1
-
-granger_pvalues_nfloods <- granger_res_nfloods$indgranger %>%
-					mutate(place_index = as.numeric(as.character(place_index))) %>%
-					select(c(place_index, `p-value`)) %>%
-					rename(`p-value_nfloods` = `p-value`)
-
-granger_pvalues_flood_dur <- granger_res_flood_dur$indgranger %>%
-					mutate(place_index = as.numeric(as.character(place_index))) %>%
-					select(c(place_index, `p-value`)) %>%
-					rename(`p-value_flood_dur` = `p-value`)
-					
-granger_pvalues_flood_area <- granger_res_flood_area$indgranger %>%
-					mutate(place_index = as.numeric(as.character(place_index))) %>%
-					select(c(place_index, `p-value`)) %>%
-					rename(`p-value_flood_area` = `p-value`)
-
-granger_pvalues_geom <- data %>%
-						mutate(time_index = as.numeric(as.factor(datetime_start)),
-								place_index = as.numeric(as.character(place_index))) %>%
-						left_join(granger_pvalues_nfloods, by = 'place_index') %>%
-						left_join(granger_pvalues_flood_dur, by = 'place_index') %>%
-						left_join(granger_pvalues_flood_area, by = 'place_index') %>%
-						filter(time_index == 16) %>%
-						mutate(sig_nfloods = case_when(`p-value_nfloods` < 0.05 ~ 'Yes',
-												`p-value_nfloods` >= 0.05 ~ 'No',
-												is.na(`p-value_nfloods`) ~ 'Not enough information'),
-								sig_flood_dur = case_when(`p-value_flood_dur` < 0.05 ~ 'Yes',
-												`p-value_flood_dur` >= 0.05 ~ 'No',
-												is.na(`p-value_flood_dur`) ~ 'Not enough information'),
-								sig_flood_area = case_when(`p-value_flood_area` < 0.05 ~ 'Yes',
-												`p-value_flood_area` >= 0.05 ~ 'No',
-												is.na(`p-value_flood_area`) ~ 'Not enough information'),) %>%
-						select(c(Name, ADMIN1, place_index, `p-value_nfloods`, `p-value_flood_dur`,
-								`p-value_flood_area`, sig_nfloods, sig_flood_dur, sig_flood_area, geom))
-								
-granger_outfile <- "../../Data/granger_results_deseasoned_diff_adm1_geom.gpkg"
+granger_outfile <- "../../Data/granger_results_test_adm2_geom_FDR.gpkg"
 st_write(granger_pvalues_geom, granger_outfile)
